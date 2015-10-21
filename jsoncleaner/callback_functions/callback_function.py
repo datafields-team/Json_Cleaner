5f094650e86d3999ccc3cabbdb640c1139168e8e
Karthik Hariharan <karthikhariharan13@gmail.com>
2015-10-21 16:11:59 -0500

    Create callback_function.py
    Abstract Parent Class in the hierarchy of callback functions that are implemented

---
+++ jsoncleaner/callback_functions/callback_function.py
@@ -0,0 +1,7 @@
+
+class CallbackFunction:
+    
+    def __init__(self, old_key, new_key, datatype=unicode):
+        self._old_key = old_key
+        self._new_key = new_key
+        self._datatype = datatype
