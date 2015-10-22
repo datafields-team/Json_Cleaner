851c82343d06e80f1869d79ac431e0a4669a2261
Karthik Hariharan <karthikhariharan13@gmail.com>
2015-10-21 22:19:45 -0500

    Added tweepy based Twitter streamer

--- examples/twitter_cleaner.py
+++ examples/twitter_cleaner.py
@@ -1,3 +1,24 @@
 from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
 from cleaner import JsonCleaner
+from auth_credentials import consumer_key, consumer_secret, access_token, access_token_secret
 
+
+class TwitterStreamer(StreamListener):
+
+    def on_data(self, raw_data):
+        print raw_data
+
+    def on_error(self, status_code):
+        print status_code
+
+
+
+if __name__ == '__main__':
+    streamer = TwitterStreamer()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    stream = Stream(auth, streamer)
+    stream.filter(track=['messi, ronaldo, hazard'])
